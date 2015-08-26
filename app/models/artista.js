2912e10a9379d2291ef61331387768a79d621836
Nahuel Garbezza <n.garbezza@gmail.com>
2015-08-26 11:19:29 -0300

    mas fixtures; async true

--- app/models/artista.js
+++ app/models/artista.js
@@ -2,13 +2,13 @@ import DS from 'ember-data';
 
 var Artista = DS.Model.extend({
   nombre: DS.attr('string'),
-  canciones: DS.hasMany('cancion', {async: false})
+  canciones: DS.hasMany('cancion', {async: true})
 });
 
 Artista.reopenClass({
   FIXTURES: [
-    {id: 1, nombre: 'Charly García'},
-    {id: 2, nombre: 'Luis A. Spinetta'}
+    {id: 1, nombre: 'Divididos', canciones: [1, 2, 3]},
+    {id: 2, nombre: 'La Renga', canciones: [4, 5]}
   ]
 });
 
--- app/models/cancion.js
+++ app/models/cancion.js
@@ -1,7 +1,21 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+var Cancion = DS.Model.extend({
   titulo: DS.attr('string'),
   puntaje: DS.attr('number'),
-  artista: DS.belongsTo('artista', {async: false})
+  artista: DS.belongsTo('artista', {async: true})
 });
+
+Cancion.reopenClass({
+  FIXTURES: [
+    // Canciones de Divididos
+    {id: 1, titulo: 'Paisano de Hurlingham', puntaje: 4},
+    {id: 2, titulo: 'Luca', puntaje: 4},
+    {id: 3, titulo: 'El Arriero', puntaje: 5},
+    // Canciones de La Renga
+    {id: 4, titulo: 'El final es en donde parti', puntaje: 4},
+    {id: 5, titulo: 'La razon que te demora', puntaje: 3}
+  ]
+});
+
+export default Cancion;
