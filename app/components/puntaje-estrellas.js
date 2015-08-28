477772d9161c3abb1fdd8fdc79142bac39977fc1
Nahuel Garbezza <n.garbezza@gmail.com>
2015-08-28 01:34:30 -0300

    rename de componente para seguir la convención (y que funcione!)

--- app/components/puntaje-estrellas.js
+++ app/components/puntaje-estrellas.js
@@ -4,20 +4,20 @@ var PuntajeComponent = Ember.Component.extend({
   classNames: ['rating-panel'],
 
   cantidadDeEstrellas: Ember.computed.alias('puntajeMaximo'),
-  todasLasEstrellas: Ember.computed.alias('puntaje'),
+  estrellasLlenas: Ember.computed.alias('puntaje'),
 
   estrellas: function () {
-    var estrellasLlenas = this.starRange(1, this.get('estrellasLlenas'), 'llena');
-    var estrellasVacias = this.starRange(this.get('estrellasLlenas') + 1, this.get('cantidadDeEstrellas'), 'vacia');
+    var estrellasLlenas = this.rangoDeEstrellas(1, this.get('estrellasLlenas'), 'llena');
+    var estrellasVacias = this.rangoDeEstrellas(this.get('estrellasLlenas') + 1, this.get('cantidadDeEstrellas'), 'vacia');
     return estrellasLlenas.concat(estrellasVacias);
-  }.property('todasLasEstrellas', 'cantidadDeEstrellas'),
+  }.property('estrellasLlenas', 'cantidadDeEstrellas'),
 
   rangoDeEstrellas: function (start, end, type) {
-    var estrellasData = [];
+    var estrellas = [];
     for (var i = start; i <= end; i++) {
-      estrellasData.push({puntaje: i, llena: type === 'llena'});
+      estrellas.push({puntaje: i, llena: type === 'llena'});
     }
-    return estrellasData;
+    return estrellas;
   },
 
   actions: {
